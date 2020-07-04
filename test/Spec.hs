4f4d0ce3e42f55ef7761028f26e99edd6f9072b4
Ignacio Slater Muñoz <roac@Ignacio-PC.localdomain>
2020-07-04 17:55:51 -0400

    Created repo

---
+++ test/Spec.hs
@@ -0,0 +1,10 @@
+-- "Mona Lista" (c) by Ignacio Slater M.
+
+-- "Mona Lista" is licensed under a
+-- Creative Commons Attribution 4.0 International License.
+
+-- You should have received a copy of the license along with this
+-- work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
