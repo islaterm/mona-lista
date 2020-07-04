2be48bda01bc8730fb36284103b9c0673969fde1
Ignacio Slater Muñoz <roac@Ignacio-PC.localdomain>
2020-07-04 18:05:49 -0400

    B1: Base definitions v1.0.0-b.1

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,4 +6,20 @@
 -- You should have received a copy of the license along with this
 -- work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
 
+-- v1.0.0-b.1
 module Lib where
+
+type Matrix = [[Int]]
+type Size = Int
+
+source :: Matrix
+source = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+
+target :: Matrix
+target = [[14, 16, 15, 13], [6, 8, 7, 5], [2, 4, 3, 1], [10, 12, 11, 9]]
+
+swapMUntil :: Size -> (Matrix -> Bool) -> (Int, [Matrix]) -> (Int, [Matrix])
+swapMUntil = undefined
+
+answer :: Int
+answer = fst $ swapMUntil 4 (== target) (0, [source])
